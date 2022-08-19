2df34a03f3b6299519b718b4c9369a3117c7a7ea
Feras <ferassaleh1980@gmail.com>
2022-08-19 16:19:15 +0200

    adding some functionalities

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import autoComplete from '../../services/Api';
+import './Header.css';
 
 const Header = () => {
     let timer = null;
+
+    const [state, setState] = useState({
+        query: '',
+        autoCompleteList: [],
+    });
+
+    const autoCompleteHandler = (query) => {
+        autoComplete(query).then((list) => {
+            setState({
+                query: query,
+                autoCompleteList: list,
+            });
+            // to remove the droped list when the user clicks outside the list .
+            if (list.length > 0) {
+                document.addEventListener('click', documentClickHandler);
+            }
+        });
+    };
+
+    const documentClickHandler = (e) => {
+        console.log(e);
+
+        setState((prevState) => ({ ...prevState, autoCompleteList: [] }));
+        document.removeEventListener('click', documentClickHandler);
+        console.log('removed');
+    };
     return (
         <div>
             <form
@@ -11,25 +38,51 @@ const Header = () => {
                     console.log('hi');
                 }}
             >
-                <select value="movie">
+                <select>
                     <option value="movie">Movies</option>
                     <option value="tv">TV Shows</option>
                 </select>
+                <div className="query-container">
+                    <input
+                        value={state.query}
+                        type="text"
+                        placeholder="Search"
+                        onChange={(e) => {
+                            setState({
+                                ...state,
+                                query: e.target.value,
+                            });
+                            // clear the previous timeout
+                            clearTimeout(timer);
 
-                <input
-                    type="text"
-                    placeholder="Search"
-                    onChange={(e) => {
-                        // clear the previous timeout
-                        clearTimeout(timer);
-                        // set a timeout to prevent the form to send so many requests
-                        timer = setTimeout(() => {
-                            autoComplete(e.target.value).then((list) =>
-                                console.log(list)
-                            );
-                        }, 2000);
-                    }}
-                />
+                            // set a timeout to prevent the form to send so many requests
+                            timer = setTimeout(() => {
+                                autoCompleteHandler(e.target.value);
+                            }, 300);
+                        }}
+                        // onFocus={(e) => {
+                        //     autoCompleteHandler(e.target.value);
+                        // }}
+                    />
+                    {state.autoCompleteList.length > 0 && (
+                        <ul className="autocomplete-results">
+                            {state.autoCompleteList.map((item) => (
+                                <li
+                                    key={item.id}
+                                    dangerouslySetInnerHTML={{
+                                        __html: item.styledName,
+                                    }}
+                                    onClick={(e) => {
+                                        setState({
+                                            query: item.name,
+                                            autoCompleteList: [],
+                                        });
+                                    }}
+                                ></li>
+                            ))}
+                        </ul>
+                    )}
+                </div>
 
                 <input type="number" placeholder="Release year" />
                 <button type="submit">Search</button>
--- src/services/Api.js
+++ src/services/Api.js
@@ -2,14 +2,24 @@ import Api_key from './Api-key';
 
 const autoComplete = (query) => {
     // check of the entered value is empty and if so returns an empty array
-    if (query === '') {
+    if (!query) {
         return Promise.resolve([]);
     }
     return fetch(
         `https://api.themoviedb.org/3/search/keyword?api_key=${Api_key}&query=${query}`
     )
         .then((response) => response.json())
-        .then((data) => data.results);
+        .then((data) =>
+            data.results.map((item) => ({
+                ...item,
+                // add a new key to style the entered value in the search field
+                styledName: item.name.replace(
+                    new RegExp(query, 'gi'),
+
+                    `<span>$&</span>`
+                ),
+            }))
+        );
 };
 
 export default autoComplete;
